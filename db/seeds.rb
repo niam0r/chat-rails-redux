cdfdd39412567fdf8fb68c335559c9fcda463c59
Romain niam0r <niamor@mm.st>
2018-07-13 19:04:25 +0100

    implemented seeds.rb, working

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,26 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Message.destroy_all
+puts "destroyed all messages"
+User.destroy_all
+puts "destroyed all users"
+Channel.destroy_all
+puts "destroyed all channels"
+
+general = Channel.create(name: 'general')
+paris = Channel.create(name: 'paris')
+react = Channel.create(name: 'react')
+
+seb = User.create(email: 'seb@lewagon.com', password: 'motdepasse')
+puts seb
+romain = User.create(email: 'romain@lewagon.com', password: 'motdepasse')
+puts romain
+boris = User.create(email: 'boris@lewagon.com', password: 'motdepasse')
+puts boris
+
+
+Message.create({user: seb, channel: general, content: "seb is testing the general channel"})
+Message.create({user: romain, channel: general, content: "romain is testing the paris channel"})
+Message.create({user: boris, channel: general, content: "boris is testing the paris channel"})
