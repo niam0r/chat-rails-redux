c7c4f64d306f2160f8e66592f2a6be1c0c670c50
Romain niam0r <niamor@mm.st>
2018-07-13 19:46:16 +0100

    implemented messages_controller

--- app/controllers/api/v1/messages_controller.rb
+++ app/controllers/api/v1/messages_controller.rb
@@ -1,7 +1,31 @@
 class Api::V1::MessagesController < ApplicationController
+  before_action :set_channel
+
   def index
+    messages = @channel.messages.order('created_at ASC')
+    render json: messages
   end
 
+  # def show
+  #   message = Message.find(params[:id])
+  #   render json: message
+  # end
+
   def create
+    message = @channel.messages.build(content: params[:content])
+    message.user = current_user
+    message.save
+    render json: message # see Message.as_json method
   end
+
+
+  private
+
+  def set_channel
+    @channel = Channel.find_by(name: params[:channel_id])
+  end
+
+  # def message_params
+  #   params.require(:message).permit(:content, :channel, :user)
+  # end
 end
