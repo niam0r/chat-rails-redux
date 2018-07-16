24a87f8e6925b83a8b3d4ace1725a026b2cee623
Romain niam0r <niamor@mm.st>
2018-07-16 16:08:47 +0100

    trying to implement #as_json, running into issues with this.props.messages.map is not a function

--- app/models/message.rb
+++ app/models/message.rb
@@ -4,20 +4,20 @@ class Message < ApplicationRecord
 
   validates :content, presence: true
 
-  after_create :broadcast_message
+  # after_create :broadcast_message
 
-  # def as_json(options = {})
+  def as_json(options = {})
 
-  #   nickname = user.nickname.nil? ? user.email.match(/[^@]+/)[0] : user.nickname
+    nickname = user.nickname.nil? ? user.email.match(/[^@]+/)[0] : user.nickname
 
-  #   {
-  #     id: id,
-  #     author: nickname,
-  #     content: content,
-  #     created_at: created_at,
-  #     channel: channel.name
-  #   }
-  # end
+    {
+      id: id,
+      author: nickname,
+      content: content,
+      created_at: created_at,
+      channel: channel.name
+    }
+  end
 
   private
 
