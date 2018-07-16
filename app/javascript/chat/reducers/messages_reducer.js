5f81803a7d253ecf40a1e73fecb1044cb962fd6f
Romain niam0r <niamor@mm.st>
2018-07-16 18:57:12 +0100

    linted and used destructuring in messages_reducer

--- app/javascript/chat/reducers/messages_reducer.js
+++ app/javascript/chat/reducers/messages_reducer.js
@@ -9,9 +9,7 @@ export default function(state = null, action) {
       if (state.map(message => message.id).includes(action.payload.id)) {
         return state;
       } else {
-        const copiedState = state.slice(0);
-        copiedState.push(action.payload);
-        return copiedState;
+        return [...state, action.payload];
       }
     case CHANNEL_SELECTED:
       return []; // Channel has changed. Clearing view.
